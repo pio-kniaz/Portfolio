bc7f494e14d08f5181606b81b4386f61895c0ea2
pio-kniaz <pio.kniaz@gmail.com>
2017-11-18 13:19:53 +0100

    navigation & header section finished

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+class MobileMenu {
+  constructor() {
+    this.hamburger=$('.header__hamburger-button')
+    this.menuContent = $('.primary-nav__ul');
+    this.events();
+
+  }
+  events(){
+    this.hamburger.click(this.toggleTheMenu.bind(this))
+  }
+  toggleTheMenu(){
+    this.menuContent.toggleClass('primary-nav__ul--active')
+    this.hamburger.toggleClass('header__hamburger-button--close-x')
+  }
+
+}
+
+export default MobileMenu;
