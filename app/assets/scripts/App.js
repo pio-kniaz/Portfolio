4f98d022892eb9bfac51747cf0dcd32f811d8bd9
pio-kniaz <pio.kniaz@gmail.com>
2017-11-25 10:23:35 +0100

    completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
+import $ from 'jquery';
 import MobileMenu from "./modules/MobileMenu"
 import ProjectsCarusele from "./modules/ProjectsCarusele"
+import RevealOnScroll from "./modules/RevealOnScroll"
 
 
 var mobileMenu = new MobileMenu();
 var projectsCarusele = new ProjectsCarusele();
-// var location = new location();
+new RevealOnScroll($('.reveal-item'),"60%");
+new RevealOnScroll($('.skill-icon'),"65%");
